3db1f86a2cc95234e63b92846e0941b15ec0a17e
Qiezi Chao <134118911+ChaoQiezi@users.noreply.github.com>
2023-06-10 10:59:35 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# @炒茄子  2023-05-15
+"""
+当前文件为运行的入口文件
+"""
+
+from 数据预处理.提取数据 import *
+from 数据预处理.整理数据 import *
+from 统计分析.Analysis import *
+from 统计分析.Plot import *
+
+if __name__ == '__main__':
+    print('程序运行完毕！')
